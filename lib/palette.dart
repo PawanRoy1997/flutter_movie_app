1fd40fe58a92d6cc009f714467974d966fa7be84
Pawan <pawanroy0066@gmail.com>
2023-12-01 14:49:04 +0530

    Initial Project with custom theme and color patelle

---
+++ lib/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/rendering.dart';
+
+class Palette{
+  static const Color almostBlack = Color(0xFF040b13);
+  static const Color red100 = Color(0xFFfce5e8);
+  static const Color red200 = Color(0xFFf7b2ba);
+  static const Color red300 = Color(0xFFf27f8d);
+  static const Color red400 = Color(0xFFec4b5f);
+  static const Color red500 = Color(0xFFe82139);
+  static const Color red600 = Color(0xFFb41326);
+  static const Color red700 = Color(0xFF800d1b);
+  static const Color red800 = Color(0xFF4d0810);
+  static const Color red900 = Color(0xFF1a0305);
+}
